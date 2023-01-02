989d7ad4471a52e80d8777822e454837a861164b
celestindeal <celestindeal42@gmail.com>
2023-01-02 10:26:25 +0100

    fix: file organization to add arduino board code

---
+++ application mobile/lib/Variable.dart
@@ -0,0 +1,7 @@
+class Variable {
+  static var url = 'http://78.117.228.233:3333/';
+  static var urlFonctionnementGeneralON = 'http://78.117.228.233:3333/ON';
+  static var urlFonctionnementGeneralOFF = 'http://78.117.228.233:3333/OFF';
+  static var urlFonctionnementGeneralFORCE = 'http://78.117.228.233:3333/FORCE';
+  static var urlregelargetemperature = 'http://78.117.228.233:3333/';
+}
